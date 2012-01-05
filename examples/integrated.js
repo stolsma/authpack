459e6995658b3d6a5971a3eae53786980dccade1
Sander Tolsma <github@tolsma.net>
2012-01-05 16:38:33 +0100

    [refactor] Example refactor

--- examples/integrated.js
+++ examples/integrated.js
@@ -8,57 +8,80 @@
  * @docauthor TTC/Sander Tolsma
  */
 
-var union = require('union');
+var director = require('director'),
+    union = require('union');
 
-var oauth2 = require('../lib/authpack').oauth2.init({
-      authentication: {},
-      authorization: {},
-      authorizationServer: {},
-      resourceServer: {}
-    });
+var oauth2 = require('../lib/authpack').oauth2;
 
-var cred = {username: 'sander', password: 'test'};
-oauth2.authentication.users.add('sander', cred, function(err, id, userData) {
-  oauth2.authorizationServer.clients.create('client', 'confidential',
-  ['http://localhost:9090/foo'], 'This is the test client', function(client) {
+// create the OAuth2 Authentication server code
+var oauth2Server = new oauth2.AuthorizationServer({
+      authentication: new oauth2.AuthenticationPlugin(),
+      authorization: new oauth2.AuthorizationPlugin()
+    }),
+    resourceServer = new oauth2.ResourceServer(),
+    cred = {username: 'sander', password: 'test'};
+
+
+// create user and client records
+oauth2Server.authentication.users.add('sander', cred, function(err, id, userData) {
+  oauth2Server.authorization.createClient('client', 'confidential',
+  ['http://localhost:9090/foo'], 'This is the test client', function(err, client) {
     console.log('userdata: ', userData);
     console.log('client.id: ', client.id);
   });
 });
 
+var router = new director.http.Router().configure({async: true});
 
-var router = oauth2.createRouter();
+//
+// authorization server endpoints
+//
+router.get('/oauth2/authorize', function(next) {
+  oauth2Server.authorizationEndpoint(this.req, this.res, next);
+});
+router.post('/oauth2/authorize', function(next) {
+  oauth2Server.authorizationEndpoint(this.req, this.res, next);
+});
+router.post('/oauth2/access_token', function(next) {
+  oauth2Server.tokenEndpoint(this.req, this.res, next);
+});
 
+//
+// authentication plugin endpoints
+//
+router.get('/login', function(next) {
+  oauth2Server.authentication.loginEndpoint(this.req, this.res, next);
+});
+router.post('/login', function(next) {
+  oauth2Server.authentication.loginEndpoint(this.req, this.res, next);
+});
+router.get('/logout', function(next) {
+  oauth2Server.authentication.logoutEndpoint(this.req, this.res, next);
+});
+
+//
+// Resource server endpoints
+//
 router.get('/foo', function () {
   this.res.writeHead(200, { 'Content-Type': 'text/plain' });
   this.res.end('hello world get\n');
 });
-
 router.post('/foo', function () {
   this.res.writeHead(200, { 'Content-Type': 'text/plain' });
   this.res.end('hello world post\n');
 });
 
-router.get('/login', function(next) {
-  oauth2.authentication.loginEndpoint(this.req, this.res, next);
-});
-
-router.post('/login', function(next) {
-  oauth2.authentication.loginEndpoint(this.req, this.res, next);
-});
-
-router.get('/logout', function(next) {
-  oauth2.authentication.logoutEndpoint(this.req, this.res, next);
-});
-
 
 var server = union.createServer({
   before: [
-    oauth2.resourceServerActions,
+    function(req, res, next) {
+      resourceServer.checkToken(req, res, next);
+    },
     function (req, res) {
       var found = router.dispatch(req, res);
       if (!found) {
-        res.emit('next');
+        res.writeHead(404);
+        res.end('Not found!');
       }
     }
   ]
